2932b68425833e6fded9ded10ac99f68ac1a2e40
cdgiessen <cdgiessen@gmail.com>
2017-01-24 23:31:59 -0600

    Added  vector 2,3,4

--- cml/cml.h
+++ cml/cml.h
@@ -1,7 +1,111 @@
 #pragma once
 namespace cml {
 
+#pragma once
 
+	class vec2 {
+	public:
+		float x;
+		float y;
+
+		vec2(float a, float b) :x(a), y(b) {
+
+		}
+
+
+
+		const vec2 &operator +=(const vec2 &val) {
+			x = x + val.x;
+			y = y + val.y;
+			return *this;
+		}
+
+		const vec2 &operator -=(const vec2 &val) {
+			x = x - val.x;
+			y = y - val.y;
+			return *this;
+		}
+
+		const vec2 &operator+(const vec2 &b) {
+			return vec2(*this) += b;
+		}
+
+		const vec2 &operator-(const vec2 &b) {
+			return vec2(*this) -= b;
+		}
+	};
+
+	class vec3 {
+	public:
+		float x;
+		float y;
+		float z;
+
+		vec3(float a, float b, float c) :x(a), y(b), z(c) {
+
+		}
+
+		const vec3 &operator +=(const vec3 &val) {
+			x = x + val.x;
+			y = y + val.y;
+			z = z + val.z;
+			return *this;
+		}
+
+		const vec3 &operator -=(const vec3 &val) {
+			x = x - val.x;
+			y = y - val.y;
+			z = z - val.z;
+			return *this;
+		}
+
+		const vec3 &operator+(const vec3 &b) {
+			return vec3(*this) += b;
+		}
+
+		const vec3 &operator-(const vec3 &b) {
+			return vec3(*this) -= b;
+		}
+	};
+
+	class vec4 {
+	public:
+		float x;
+		float y;
+		float z;
+		float w;
+		vec4(float a, float b, float c, float d) :x(a), y(b), z(c), w(d) {
+
+		}
+
+		vec4(float a, float b, float c) :x(a), y(b), z(c) {
+
+		}
+
+		const vec4 &operator +=(const vec4 &val) {
+			x = x + val.x;
+			y = y + val.y;
+			z = z + val.z;
+			w = w + val.w;
+			return *this;
+		}
+
+		const vec4 &operator -=(const vec4 &val) {
+			x = x - val.x;
+			y = y - val.y;
+			z = z - val.z;
+			w = w - val.w;
+			return *this;
+		}
+
+		const vec4 &operator+(const vec4 &b) {
+			return vec4(*this) += b;
+		}
+
+		const vec4 &operator-(const vec4 &b) {
+			return vec4(*this) -= b;
+		}
+	};
 
 
 
