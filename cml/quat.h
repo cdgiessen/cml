4bf2af3a2911fabeb0cc1fd9665ba8e1cc86ea3d
DESKTOP-HOME\Charles <cdgiessen@gmail.com>
2017-02-23 12:52:24 -0600

    Worked on most of quaternion stuff. Just the meat is left (rotations)

--- cml/quat.h
+++ cml/quat.h
@@ -18,12 +18,13 @@ angle - Returns the angle in degrees between two rotations a and b.
 
 axisangles - Creates a rotation which rotates angle degrees around axis.
 dot - The dot product between two rotations.
-inverse - Returns the Inverse of rotation.
+#inverse - Returns the Inverse of rotation.
 rotate towards - 	Rotates a rotation from towards to.
 
 
 Operators
-== != *(combine lhs with rhs)
+# + += - -= * 
+# == != *(combine lhs with rhs)
 */
 
 namespace cml {
@@ -31,19 +32,140 @@ namespace cml {
 	template<typename T>
 	class quat {
 	public:
-		T x;
-		T y;
-		T z;
-		T w;
+		T x; // Imaginary x
+		T y; // Imaginary y
+		T z; // Imaginary z
+		T w; // Real Part
 
+		//CONSTRUCTORS
+
+		//Default constructor
+		quat() : x(0), y(0), z(0), w(0) {}
+
+		//Constructor from arguments
 		quat(T a, T b, T c, T d) : x(a), y(b), z(c), w(d) {}
 
-	private:
+		//Copy constructor
+		quat(const quat<T>& copy) : x(copy.x), y(copy.y), z(copy.z), w(copy.w) {}
 
+		//Constructor from vector and real part
+		quat(const vec3<T> imagVec, const T realVal) : x(imagVec.x), y(imagVec.y), z(imagVec.z), w(realVal) {}
+
+		//Returns a vector of the imaginary part of a quaternion
+		vec3<T> getImag() {
+			return vec3<T>(x, y, z);
+		}
+
+		//REturns the real part of a quaternion
+		T getReal() {
+			return w;
+		}
+
+		//OPERATORS
+
+		//Quaternion Addition
+		quat<T> operator+ (const quat<T> val) const {
+			return quat<T>(x + val.x, y + val.y, z + val.z, w + val.w);
+		}
+
+		//Addition
+		quat<T>& operator+= (const quat<T> val) {
+			x += val.x;
+			y += val.y;
+			z += val.z;
+			w += val.w;
+			return *this;
+		}
+
+		//Quaternion Subtraction
+		quat<T> operator- (const quat<T> val) const {
+			return quat<T>(x - val.x, y - val.y, z - val.z, w - val.w);
+		}
+
+		//Subtraction
+		quat<T>& operator-= (const quat<T> val) {
+			x -= val.x;
+			y -= val.y;
+			z -= val.z;
+			w -= val.w;
+			return *this;
+		}
+
+		//Scalar Multiplication
+		quat<T> operator *(const T val) const {
+			return quat<T>(val * x, val * y, val * z, val * w);
+		}
+
+		//Quaternion multiplication
+		quat<T> operator * (const quat<T> val) const {
+			return quat<T>(w * val.x - x * val.w + y * val.z - z * val.y, 
+						   w * val.y - x * val.z + y * val.w + z * val.x, 
+						   w * val.z + x * val.y - y * val.x + z * val.w , 
+						   w * val.w - x * val.x - y * val.y - z * val.z);
+		}
+
+		//EQUALITY
+		bool operator ==(const quat<T>& val) const {
+			return (cmpf(x, val.x) && cmpf(y, val.y) && cmpf(z, val.z) && cmpf(w, val.w));
+		}
+
+		bool operator !=(const quat<T>& val) const {
+			return !(*this == val);
+		}
+
+		//Negation
+		quat<T> operator-() const{
+			return quat<T>(-x, -y, -z, -w);
+		}
+
+		//Conjugate, only inverts the imaginary portion
+		quat<T> operator~() const {
+			return quat<T>(-x, -y, -z, w);
+		}
+
+		//MAGNITUDE
+		T mag() const {
+			return (T)std::sqrt(x*x + y*y + z*z + w*w);
+		}
+
+		T magSqrd(void) const {
+			return (x*x + y*y + z*z + w*w);
+		}
+
+		//Normalize
+		void norm() {
+			T mag = (*this).mag();
+			x /= mag;
+			y /= mag;
+			z /= mag;
+			w /= mag;
+		}
+
+		//normalizes a given quaternion
+		quat<T>& norm(quat<T>& val) const {
+			T mag = val.mag();
+			val.x /= mag;
+			val.y /= mag;
+			val.z /= mag;
+			val.w /= mag;
+			return (val);
+		}
+
+		//Gets an inverse quaternion of this one 
+		quat<T> inverse() {
+			T mag = (*this).magSqrd();
+			return quat<T>(-x / mag, -y / mag, -z / mag, w / mag);
+		}
+
+		//Lerp TODO
+		//quat<T> lerp(T factor, const quat<T>& val) const
+		//{
+		//	return quat<T>((*this).getImag().lerp(factor,val), (1 - factor) * w + factor * val.w);
+		//}
 
 		//put to output
 		friend std::ostream& operator<<(std::ostream &strm, const quat<T> &q) {
-			return strm << "[" << q.x << ", " << q.y << ", " << q.z << ", " << q.w << "]";
+			return strm << "[(" << q.x << ", " << q.y << ", " << q.z << "), " << q.w << "]";
 		}
 
 		//To string
@@ -58,6 +180,18 @@ namespace cml {
 	typedef quat<double> quatd;
 
 	static const quat<float> IDENTITY(0, 0, 0, 1);
+
+	static const quat<float> QUAT_ABOUT_X_90(std::sqrt(0.5), 0, 0, std::sqrt(0.5));
+	static const quat<float> QUAT_ABOUT_X_180(1, 0, 0, 0);
+	static const quat<float> QUAT_ABOUT_X_270(-std::sqrt(0.5), 0, 0, std::sqrt(0.5));
+
+	static const quat<float> QUAT_ABOUT_Y_90(0, std::sqrt(0.5), 0, std::sqrt(0.5));
+	static const quat<float> QUAT_ABOUT_Y_180(0, 1, 0, 0);
+	static const quat<float> QUAT_ABOUT_Y_270(0, -std::sqrt(0.5), 0, std::sqrt(0.5));
+
+	static const quat<float> QUAT_ABOUT_Z_90(0, 0, std::sqrt(0.5), std::sqrt(0.5));
+	static const quat<float> QUAT_ABOUT_Z_180(0, 0, 1, 0);
+	static const quat<float> QUAT_ABOUT_Z_270(0, 0, -std::sqrt(0.5), std::sqrt(0.5));
 
 }
 
